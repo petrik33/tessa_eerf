a839d494cd498a36e072b0ed95009ca5ec48e02b
Tsimafei Petrykevich <petrik3003@gmail.com>
2025-09-13 13:46:13 +0300

    extract base interface of visual unit and add new implementation

---
+++ script/combat/visual/combat_visual_unit_base.gd
@@ -0,0 +1,11 @@
+@tool
+class_name CombatVisualUnitBase extends Node2D
+
+
+func get_physical_node() -> Node2D:
+	assert(false, "not implemented")
+	return null
+
+
+func execute(action: CombatVisualUnitActionBase):
+	assert(false, "not implemented")
