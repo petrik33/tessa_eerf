b449975e6ea9c11558959a14e916ab091809e75a
Timofey Petrikevich <petrik3003@gmail.com>
2025-11-18 23:43:45 +0300

    unfiltered: progress through 18.11

--- script/combat/dbg_combat_controller.gd
+++ script/combat/dbg_combat_controller.gd
@@ -2,6 +2,7 @@ class_name DbgCombatController extends Node
 
 @export var combat: Combat
 @export var coordinates: HexGridRendererBase
+@export var outlines: Node2D
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("dbg_start_combat"):
@@ -10,6 +11,8 @@ func _unhandled_input(event: InputEvent) -> void:
 		combat.finish()
 	elif event.is_action_pressed("dbg_toggle_hex_coordinates"):
 		coordinates.visible = not coordinates.visible
+	elif event.is_action_pressed("dbg_toggle_outlines"):
+		outlines.visible = not outlines.visible
 	elif event.is_action_pressed("dbg_toggle_fullscreen"):
 		var window_mode := DisplayServer.window_get_mode()
 		if window_mode == DisplayServer.WINDOW_MODE_WINDOWED:
